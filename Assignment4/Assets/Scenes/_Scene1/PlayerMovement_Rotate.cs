49b831f7061f7981c76d59180fe10b766366069e
mm16bn <32907153+mm16bn@users.noreply.github.com>
2018-09-14 01:10:22 -0400

    second commit with scene 1

---
+++ Assignment4/Assets/Scenes/_Scene1/PlayerMovement_Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_Rotate : MonoBehaviour {
+
+    public float RotateSpeed = 30f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(-Vector3.up * RotateSpeed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up * RotateSpeed * Time.deltaTime);
+    }
+}
