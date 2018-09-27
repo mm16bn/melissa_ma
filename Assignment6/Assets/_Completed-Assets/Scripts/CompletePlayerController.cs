2cd16bf697f04d874f9f46c43d1def7865f6451d
mm16bn <32907153+mm16bn@users.noreply.github.com>
2018-09-27 10:45:05 -0400

    added score counter and point system

--- Assignment6/Assets/_Completed-Assets/Scripts/CompletePlayerController.cs
+++ Assignment6/Assets/_Completed-Assets/Scripts/CompletePlayerController.cs
@@ -7,11 +7,15 @@ using UnityEngine.UI;
 public class CompletePlayerController : MonoBehaviour {
 
 	public float speed;				//Floating point variable to store the player's movement speed.
-	public Text countText;			//Store a reference to the UI Text component which will display the number of pickups collected.
-	public Text winText;			//Store a reference to the UI Text component which will display the 'You win' message.
+	public Text starsText;		    //Store a reference to the UI Text component which displays the number of stars collected.
+    public Text meteorsText;        //Store a reference to the UI text component which displays the number of meteors collected.
+    public Text scoreText;          //Stores a reference ot the UI text component which displays the total score.
+    public Text winText;			//Store a reference to the UI Text component which will display the 'You win' message.
 
 	private Rigidbody2D rb2d;		//Store a reference to the Rigidbody2D component required to use 2D Physics.
-	private int count;				//Integer to store the number of pickups collected so far.
+    private int count_1;			//Integer to store the number of pickup 1(stars) collected so far.
+    private int count_2;            //Integer to store the number of pickup 2(meteors) collected so far. 
+    private int score;              //Integer to store the total score where pickup 1 is 1 pont and pickup 2 5 points.
 
 	// Use this for initialization
 	void Start()
@@ -19,8 +23,11 @@ public class CompletePlayerController : MonoBehaviour {
 		//Get and store a reference to the Rigidbody2D component so that we can access it.
 		rb2d = GetComponent<Rigidbody2D> ();
 
-		//Initialize count to zero.
-		count = 0;
+		//Initialize count of first pickup to zero.
+		count_1 = 0;
+
+        //Initialize count of second pickup to zero.
+        count_2 = 0;
 
 		//Initialze winText to a blank string since we haven't won yet at beginning.
 		winText.text = "";
@@ -55,11 +62,20 @@ public class CompletePlayerController : MonoBehaviour {
 			other.gameObject.SetActive(false);
 			
 			//Add one to the current value of our count variable.
-			count = count + 1;
+			count_1 = count_1 + 1;
 			
 			//Update the currently displayed count by calling the SetCountText function.
 			SetCountText ();
 		}
+
+        if (other.gameObject.CompareTag("Pickup2"))
+        {
+            other.gameObject.SetActive(false);
+            count_2 = count_2 + 1;
+            SetCountText(); 
+        }
+
+
 		
 
 	}
@@ -67,12 +83,18 @@ public class CompletePlayerController : MonoBehaviour {
 	//This function updates the text displaying the number of objects we've collected and displays our victory message if we've collected all of them.
 	void SetCountText()
 	{
-		//Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
-		countText.text = "Count: " + count.ToString ();
+        //Sets pickup1 to 1 point, and pickup2 to 2 points. 
+        score = count_1 + (count_2 * 5);
+
+        //Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
+        starsText.text = "Stars: " + count_1.ToString ();
+        meteorsText.text = "Meteors: " + count_2.ToString();
+        scoreText.text = "SCORE: " + score.ToString();
+
 
 		//Check if we've collected all 12 pickups. If we have...
-		if (count >= 12)
-			//... then set the text property of our winText object to "You win!"
-			winText.text = "You win!";
+		//if (count >= 12)
+			////... then set the text property of our winText object to "You win!"
+			//winText.text = "You win!";
 	}
 }
